91f67643b6940348d94066f2619faff4d990121f
Mateus Faria <mateusfariadomingos@hotmail.com>
2022-12-06 00:43:49 -0400

    👌 IMPROVE: upgrade xlxs converter

--- excel.js
+++ excel.js
@@ -1,44 +1,21 @@
-let selectedFile;
-//console.log(window.XLSX);
-document.getElementById("input").addEventListener("change", (event) => {
-  selectedFile = event.target.files[0];
-});
-
-let data = [
-  {
-    name: "jayanth",
-    data: "scd",
-    abc: "sdef",
-  },
-];
-
-let participantsList;
-
-document.getElementById("button").addEventListener("click", () => {
-  XLSX.utils.json_to_sheet(data, "out.xlsx");
-  if (selectedFile) {
-    let fileReader = new FileReader();
+function excelToJSON(selectedFile) {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
     fileReader.readAsBinaryString(selectedFile);
     fileReader.onload = (event) => {
-      let data = event.target.result;
-      let workbook = XLSX.read(data, { type: "binary" });
-      //console.log(workbook);
-      workbook.SheetNames.forEach((sheet) => {
-        let rowObject = XLSX.utils.sheet_to_row_object_array(
-          workbook.Sheets[sheet]
-        );
-        participantsList = rowObject;
-        console.log(participantsList);
-        document.getElementById("jsondata").innerHTML = JSON.stringify(
-          rowObject,
-          undefined,
-          4
-        );
-      });
+      const data = event.target.result;
+      const workbook = XLSX.read(data, { type: "binary" });
+      const sheet = workbook.SheetNames[0];
+      const participantsList = XLSX.utils.sheet_to_row_object_array(
+        workbook.Sheets[sheet]
+      );
+      resolve(participantsList);
     };
-  }
-});
+  });
+}
 
 function excelDateToJsDate(excelDate) {
   return new Date((excelDate - (25567 + 1)) * 86400 * 1000);
 }
+
+export { excelToJSON, excelDateToJsDate };
