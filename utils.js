185907524da612d3e598f6b17bec6a088870f7aa
Mateus Faria <mateusfariadomingos@hotmail.com>
2022-12-07 00:15:12 -0400

    📦 NEW: create utils to shuffle and list combats

---
+++ utils.js
@@ -0,0 +1,45 @@
+function embaralha(array) {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  // Enquanto ainda houver elementos para embaralhar
+  while (currentIndex != 0) {
+    // Pega um elemento que sobrou
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // E troca com o elemento atual
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+function listarConfrontos(participantes) {
+  let elementosParaRenderizar = "";
+  for (let i = 0; i < participantes.length; i += 2) {
+    const pessoa1 = participantes[i];
+    const pessoa2 = participantes[i + 1];
+    elementosParaRenderizar += `<p><span>${pessoa1.Nome}</span> X <span>${
+      pessoa2?.Nome ?? "Sem oponente"
+    }</span></p>`;
+  }
+  return elementosParaRenderizar;
+}
+
+function retornarIdade(nascimento) {
+  const nascimentoFormatado = new Date(
+    (nascimento - (25567 + 1)) * 86400 * 1000
+  );
+
+  const hoje = new Date();
+
+  const tempo = hoje - nascimentoFormatado;
+
+  return tempo / (1000 * 60 * 60 * 24 * 365);
+}
+
+export { embaralha, listarConfrontos, retornarIdade };
