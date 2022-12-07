9c1c2e4fc0c2678c5e91a6fa481411cdeffa9be3
Mateus Faria <mateusfariadomingos@hotmail.com>
2022-12-07 00:17:17 -0400

    📦 NEW: show column with combats

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { excelToJSON, excelDateToJsDate } from "./excel.js";
+import { excelToJSON } from "./excel.js";
 import {
   abaixoCinco,
   deSeisANove,
@@ -7,13 +7,21 @@ import {
   acimaDezoito,
 } from "./idades.js";
 
-let selectedFile;
-document.getElementById("input").addEventListener("change", (event) => {
-  selectedFile = event.target.files[0];
-});
+import { embaralha } from "./utils.js";
 
-document.getElementById("button").addEventListener("click", async () => {
+const coluna1 = document.getElementById("abaixo-cinco");
+
+let selectedFile;
+document
+  .getElementById("entrada-arquivo")
+  .addEventListener("change", (event) => {
+    selectedFile = event.target.files[0];
+  });
+
+document.getElementById("sorteador").addEventListener("click", async () => {
   const participantes = await excelToJSON(selectedFile);
+
+  embaralha(participantes);
 
   const abaixo5 = abaixoCinco(participantes);
   const de6a9 = deSeisANove(participantes);
@@ -21,11 +29,13 @@ document.getElementById("button").addEventListener("click", async () => {
   const de14a17 = deCatorzeADezessete(participantes);
   const acima18 = acimaDezoito(participantes);
 
-  console.log(abaixo5);
-  console.log(de6a9);
-  console.log(de10a13);
-  console.log(de14a17);
-  console.log(acima18);
+  coluna1.innerHTML = de14a17;
+
+  // console.log(abaixo5);
+  // console.log(de6a9);
+  // console.log(de10a13);
+  // console.log(de14a17);
+  // console.log(acima18);
   // const participantesM = participantes.filter(
   //   (pessoa) => pessoa.Sexo === "MASCULINO"
   // );
